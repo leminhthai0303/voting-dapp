6341dec30877a07d4bef49f00c2443ca24c33062
ng0cph0ng <93986136+ng0cph0ng@users.noreply.github.com>
2024-08-26 18:50:41 +0700

    Create 2_deploy_contracts.js

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,6 @@
+const Voting = artifacts.require("Voting");
+ 
+module.exports = function(deployer, network, accounts) {
+    const privilegedVoters = accounts.slice(0, 5);
+    deployer.deploy(Voting, privilegedVoters);
+};
