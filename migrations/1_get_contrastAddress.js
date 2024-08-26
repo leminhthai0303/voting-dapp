47fca1a75117f4f83c85f33134bf72808ee679c2
ng0cph0ng <93986136+ng0cph0ng@users.noreply.github.com>
2024-08-26 21:45:42 +0700

    Delete migrations/1_get_contrastAddress.js

--- migrations/1_get_contrastAddress.js
+++
@@ -1,6 +0,0 @@
-const MyContract = artifacts.require("MyContract");
-
-module.exports = async function (deployer, network, accounts) {
-  const instance = await MyContract.deployed();
-  console.log("Contract address:", instance.address);
-};
