8562009c7d5abedbfebdec49068ce6ecca94286a
ng0cph0ng <93986136+ng0cph0ng@users.noreply.github.com>
2024-08-26 21:45:27 +0700

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,8 @@
 const Voting = artifacts.require("Voting");
  
 module.exports = function(deployer, network, accounts) {
+    const instance = await MyContract.deployed();
+    console.log("Contract address:", instance.address);
     const privilegedVoters = accounts.slice(0, 5);
     deployer.deploy(Voting, privilegedVoters);
 };
