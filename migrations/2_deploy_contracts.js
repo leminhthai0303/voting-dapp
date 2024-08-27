20fa62a96003b5c32bd363157e20a74007a801c3
LeMiTa <leminhthai0303@gmail.com>
2024-08-27 10:42:13 +0700

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,6 @@
 const Voting = artifacts.require("Voting");
 
 module.exports = function(deployer, network, accounts) {
-    const instance = Voting.deployed();
-    console.log("Contract address:", instance.address);
     const privilegedVoters = accounts.slice(0, 5);
     deployer.deploy(Voting, privilegedVoters);
 };
